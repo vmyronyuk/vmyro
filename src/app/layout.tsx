cdb59302a8d9e66b330a2fcc93a6b77aa9542ed5
vmyronyuk <vitalikmur38@gmail.com>
2025-06-14 17:06:17 +0300

    fix dark theme, update ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import { Footer } from '@/features/globals/components/Footer'
 import { Header } from '@/features/globals/components/Header/Header'
 import type { Metadata } from 'next'
-import { ThemeProvider } from 'next-themes'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
+import { Providers } from './providers'
 
 const geistSans = Geist({
 	variable: '--font-geist-sans',
@@ -26,15 +26,15 @@ export default function RootLayout({
 	children: React.ReactNode
 }>) {
 	return (
-		<html lang='en'>
+		<html lang='en' suppressHydrationWarning>
 			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+				className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background`}
 			>
-				<ThemeProvider>
+				<Providers>
 					<Header />
 					{children}
 					<Footer />
-				</ThemeProvider>
+				</Providers>
 			</body>
 		</html>
 	)

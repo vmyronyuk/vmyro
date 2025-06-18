690f7e59f8d1b9a7d6298f7d3c615756ee662dd3
vmyronyuk <vitalikmur38@gmail.com>
2025-06-18 20:08:04 +0300

    add basic filtration

--- src/features/projects/components/Projects.tsx
+++ src/features/projects/components/Projects.tsx
@@ -1,9 +1,14 @@
+'use client'
+
+import { useState } from 'react'
 import { projects } from '../utils/project'
 import { ProjectCard } from './ProjectCard/ProjectCard'
 import { ProjectsFilter } from './ProjectsFilter'
 import { ProjectsWrapper } from './ProjectsWrapper'
 
 export function Projects() {
+	const [filteredProjects, setFilteredProjects] = useState(projects)
+
 	return (
 		<section id='projects' className='py-20 w-full container'>
 			<div className='px-4 text-center flex flex-col gap-3'>
@@ -11,10 +16,13 @@ export function Projects() {
 				<p className='text-muted-foreground text-lg'>Some of my recent work</p>
 			</div>
 			<div className='flex flex-col gap-12'>
-				<ProjectsFilter />
+				<ProjectsFilter
+					projects={projects}
+					setFilteredProjects={setFilteredProjects}
+				/>
 
 				<ProjectsWrapper>
-					{projects.map(project => (
+					{filteredProjects.map(project => (
 						<ProjectCard key={project.title} project={project} />
 					))}
 				</ProjectsWrapper>
--- src/features/projects/components/ProjectsFilter.tsx
+++ src/features/projects/components/ProjectsFilter.tsx
@@ -1,20 +1,64 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { debounce } from 'lodash'
+import { useCallback, useMemo, useState } from 'react'
 import { projectCategoriesOptions } from '../domain'
+import { Project, ProjectCategory } from '../types'
+type ProjectsFilterProps = {
+	projects: Project[]
+	setFilteredProjects: (projects: Project[]) => void
+}
 
-export function ProjectsFilter() {
+export function ProjectsFilter({
+	projects,
+	setFilteredProjects,
+}: ProjectsFilterProps) {
+	const [selectedCategory, setSelectedCategory] =
+		useState<ProjectCategory>('all')
+
+	const debouncedNameFilter = useMemo(() => {
+		return debounce((name: string) => {
+			const filteredProjects = projects.filter(project =>
+				project.title.toLowerCase().includes(name.toLowerCase())
+			)
+			setFilteredProjects(filteredProjects)
+		}, 300)
+	}, [projects, setFilteredProjects])
+
+	const nameFilterHandler = useCallback(
+		(e: React.ChangeEvent<HTMLInputElement>) => {
+			debouncedNameFilter(e.target.value)
+		},
+		[debouncedNameFilter]
+	)
+
+	const categoryFilterHandler = useCallback(
+		(category: ProjectCategory) => {
+			setSelectedCategory(category)
+			const filteredProjects =
+				category === 'all'
+					? projects
+					: projects.filter(project => project.category === category)
+			setFilteredProjects(filteredProjects)
+		},
+		[projects, setFilteredProjects]
+	)
+
 	return (
 		<div className='flex flex-col gap-6 mt-9'>
 			<div className='mx-auto w-xs md:w-md'>
-				<Input placeholder='Search project...' />
+				<Input placeholder='Search project...' onChange={nameFilterHandler} />
 			</div>
 			<div className='flex flex-wrap justify-center gap-2'>
 				{projectCategoriesOptions.map(category => (
 					<Button
 						key={category.value}
-						variant={'outline'}
+						variant={
+							selectedCategory === category.value ? 'default' : 'outline'
+						}
 						size={'sm'}
 						className='font-semibold'
+						onClick={() => categoryFilterHandler(category.value)}
 					>
 						{category.label}
 					</Button>

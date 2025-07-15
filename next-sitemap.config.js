e040a0801553b54036c3c24b1ca60873001fdca7
Vitaliy Myroniuk <vitalikmur38@gmail.com>
2025-07-15 20:17:31 +0300

    [VMY-8]: add seo and analytics

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+	siteUrl: 'https://www.vmyro.co/',
+	generateRobotsTxt: true,
+	generateIndexSitemap: false,
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import { Footer } from '@/features/globals/components/Footer'
 import { Header } from '@/features/globals/components/Header/Header'
+import { Analytics } from '@vercel/analytics/next'
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
 import { Providers } from './providers'
 
@@ -17,12 +19,28 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
 	title:
-		'Vitaliy Myroniuk — Fullstack Developer | 2 Years Experience | Next.js, React, Node.js',
+		'Vitaliy Myroniuk — Fullstack Developer | Next.js, React, Node.js Expert',
 	description:
 		'Vitaliy Myroniuk — experienced Fullstack Developer with 2 years working on Next.js, React, Node.js, and AI-powered web applications. Building scalable, modern solutions using cutting-edge technologies. Let’s create your project together!',
 	icons: {
 		icon: '/favicon.svg',
 	},
+	openGraph: {
+		title: 'Vitaliy Myroniuk — Fullstack Developer',
+		description:
+			'Explore the portfolio of a modern Fullstack Developer. Built with Next.js, React, TypeScript, and more.',
+		url: 'https://www.vmyro.co/',
+		siteName: 'Vitaliy Myroniuk',
+		locale: 'en_US',
+		type: 'website',
+	},
+	twitter: {
+		card: 'summary_large_image',
+		title: 'Vitaliy Myroniuk — Fullstack Developer',
+		description:
+			'Building modern fullstack solutions using Next.js, React, Node.js, TypeScript.',
+	},
+	metadataBase: new URL('https://www.vmyro.co/'),
 }
 
 export default function RootLayout({
@@ -32,6 +50,38 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang='en' suppressHydrationWarning>
+			<head>
+				<link rel='preconnect' href='https://fonts.googleapis.com' />
+				<link
+					rel='preconnect'
+					href='https://fonts.gstatic.com'
+					crossOrigin=''
+				/>
+
+				<Script
+					id='ld-json'
+					type='application/ld+json'
+					strategy='afterInteractive'
+					dangerouslySetInnerHTML={{
+						__html: JSON.stringify({
+							'@context': 'https://schema.org',
+							'@type': 'Person',
+							name: 'Vitaliy Myroniuk',
+							jobTitle: 'Fullstack Developer',
+							url: 'https://www.vmyro.co/',
+							sameAs: [
+								'https://github.com/vmyronyuk',
+								'https://ua.linkedin.com/in/vitaliymyronyuk',
+								'https://www.instagram.com/v.myroniuk/',
+							],
+						}),
+					}}
+				/>
+				<meta
+					name='google-site-verification'
+					content='UogrYoaEWoO1MG1jkpUTWkPbSBFoZk3iVwaN3Hbioq0'
+				/>
+			</head>
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background`}
 			>
@@ -39,6 +89,7 @@ export default function RootLayout({
 					<Header />
 					{children}
 					<Footer />
+					<Analytics />
 				</Providers>
 			</body>
 		</html>

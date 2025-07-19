166d0b3092388a1f38af7ba6b407072b35e568ed
Vitaliy Myroniuk <vitalikmur38@gmail.com>
2025-07-19 14:57:21 +0300

    [VMY-2]: add payload types

--- payload.config.ts
+++ payload.config.ts
@@ -1,14 +1,22 @@
-import { projectsCollection } from '@/features/projects/collections/projectsCollection'
 import { postgresAdapter } from '@payloadcms/db-postgres'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import path from 'path'
 import { buildConfig } from 'payload'
 import sharp from 'sharp'
+import { fileURLToPath } from 'url'
+import { projectsCollection } from './src/features/projects/collections/projectsCollection'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
 
 export default buildConfig({
 	editor: lexicalEditor(),
 	collections: [projectsCollection],
 
 	secret: process.env.PAYLOAD_SECRET || '',
+	typescript: {
+		outputFile: path.resolve(dirname, 'payload-types.ts'),
+	},
 	db: postgresAdapter({
 		pool: {
 			connectionString: process.env.DATABASE_URL,
